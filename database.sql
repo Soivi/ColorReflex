463ae2e6e37ab90306e79ba6d5784deb226da9cc
Lauri Soivi <soivi.lauri@gmail.com>
2014-05-02 20:18:38 +0300

    database.sql added. Database create statement and example data are in this file

---
+++ database.sql
@@ -0,0 +1,14 @@
+# HighscoresTable
+CREATE TABLE highscores (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), time DOUBLE);
+
+# TestData
+INSERT INTO highscores (name,time) VALUES('Jane', 100.89);
+INSERT INTO highscores (name,time) VALUES('Jill', 110);
+INSERT INTO highscores (name,time) VALUES('Walter', 115.89);
+INSERT INTO highscores (name,time) VALUES('Eve', 135.89);
+INSERT INTO highscores (name,time) VALUES('John', 143.89);
+INSERT INTO highscores (name,time) VALUES('Adam', 146.89);
+INSERT INTO highscores (name,time) VALUES('Aden', 157.89);
+INSERT INTO highscores (name,time) VALUES('Steven', 240.89);
+INSERT INTO highscores (name,time) VALUES('Santos', 156.89);
+INSERT INTO highscores (name,time) VALUES('Alexis', 277.89);

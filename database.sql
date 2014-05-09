7db9b6b3f677455c33b636088b3ee7e701531393
Lauri Soivi <soivi.lauri@gmail.com>
2014-05-09 19:47:19 +0300

    database.sql changes

--- database.sql
+++ database.sql
@@ -4,11 +4,11 @@ CREATE TABLE highscores (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHA
 # TestData
 INSERT INTO highscores (name,time) VALUES('Jane', 100.89);
 INSERT INTO highscores (name,time) VALUES('Jill', 110);
-INSERT INTO highscores (name,time) VALUES('Walter', 115.89);
-INSERT INTO highscores (name,time) VALUES('Eve', 135.89);
-INSERT INTO highscores (name,time) VALUES('John', 143.89);
-INSERT INTO highscores (name,time) VALUES('Adam', 146.89);
-INSERT INTO highscores (name,time) VALUES('Aden', 157.89);
-INSERT INTO highscores (name,time) VALUES('Steven', 240.89);
-INSERT INTO highscores (name,time) VALUES('Santos', 156.89);
-INSERT INTO highscores (name,time) VALUES('Alexis', 277.89);
+INSERT INTO highscores (name,time) VALUES('Walter', 115.9);
+INSERT INTO highscores (name,time) VALUES('Eve', 135.42);
+INSERT INTO highscores (name,time) VALUES('John', 143.6);
+INSERT INTO highscores (name,time) VALUES('Adam', 146.49);
+INSERT INTO highscores (name,time) VALUES('Aden', 157.12);
+INSERT INTO highscores (name,time) VALUES('Steven', 240.85);
+INSERT INTO highscores (name,time) VALUES('Santos', 156.43);
+INSERT INTO highscores (name,time) VALUES('Alexis', 277.57);
